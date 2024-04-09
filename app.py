ae6a14a112f6ca0bf013abfa36c7256b74226d7a
NCNIPC梅苑 <64565724+xulei1112@users.noreply.github.com>
2024-04-09 16:45:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,10 +18,24 @@ def exchange():
     code = request.args.get('code') #1.用户提交充值码
  
     flag = card.query.filter_by(cardid=code).first() #数据库查询该码
-    if not flag: #表示无该充值码或该码已被消费过
-            return "helleo"
+    if flag.cardused=="true": #表示无该充值码或该码已被消费过
+            return "yijingyongle"
+    elif flag.cardused=="false": 
+            flag.cardused="true"
+            db.session.commit()
+            return "now used"
+    else:
+            return "404"
+@app.route('/exchange2', methods=['GET','POST'])
+def exchange2():
+    code = request.args.get('code') #1.用户提交充值码
+    flag = card.query.filter_by(cardid=code).first() #数据库查询该码
+    if flag.cardused=="false": #表示无该充值码或该码已被消费过
+            return "yijingyongle"
     else: 
-            return "sdasdasd"
+            flag.cardused="false"
+            db.session.commit()
+            return "now used"
  
 if __name__ == '__main__':  
     app.run()   
